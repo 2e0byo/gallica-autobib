2ce397f9e3516bd0453bede3b1615d68ed7968d8
John Maximilian <2e0byo@gmail.com>
2021-05-06 18:47:35 +0100

    handling some things!

--- gallica_autobib/module.py
+++ gallica_autobib/module.py
@@ -3,67 +3,69 @@ from pydantic import BaseModel, Field
 from typing import Optional, Literal, Union, List
 from .query import assemble_query
 
+record_types = {
+    "Article": None,
+    "Journal": "per",
+    "Book": "mon",
+    "Collection": "col"
+    # "Collection:", ["rec", "col", "ens"]
+}
+
 
 class BibBase(BaseModel):
     """Properties shared with all kinds of bibliographic items."""
 
     publicationdate: int = Field(None, alias="year")
     publisher: str = None
-    title: str = None
 
     def _source(self):
         return self
 
+    def translate(self):
+        data = self.dict(exclude={"editor"})
+
     def get_query(self) -> str:
         """Get query str"""
         exclude = {"editor"}
-        data = self._source().dict(by_alias=False, exclude=exclude)
-        data["record_type"] = self._source().record_type
+        source = self._source()
+        data = source.translate()
+        data["recordtype"] = record_types[type(source).__name__]
+        print(data)
 
-        data = {k: v for k, v in data if v}
+        data = {f"bib.{k}": v for k, v in data.items() if v}
 
-        return assemble_query(data)
+        return assemble_query(**data)
 
 
 class Article(BibBase):
     """An article."""
 
-    journal_title: str = None
+    title: str
+    journal_title: str
     pages: Union[int, List]
     author: str
     editor: str = None
 
     def _source(self):
-        if not self.source:
-            self.source = Journal.parse_obj(self.dict())
-        return self.source
+        return Journal.parse_obj(self.dict())
 
 
 class Book(BibBase):
+    title: str
     author: str
     editor: str = None
-
-    def _source(self):
-        self.recordtype = "mon"
-        return self
 
 
 class Collection(BibBase):
+    title: str
     author: str
     editor: str = None
 
-    def _source(self):
-        self.recordtype = ["rec", "col", "ens"]
-        return self
-
 
 class Journal(BibBase):
-    def _source(self):
-        self.recordtype = "per"
-        return self
+    journal_title: str
 
-
-class BibRecord(BaseModel):
-    """Model to represent a bibliographic record."""
-
-    recordtype: Literal["mon", "per", "rec", "col", "ens", "his"] = Field(alias="type")
+    def translate(self):
+        data = self.dict(exclude={"journal_title"})
+        data["title"] = self.journal_title
+        return data
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,7 +1,31 @@
-# package imports
-from gallica-autobib.module import Human
+import pytest
+from gallica_autobib.module import Article, Book, Collection, Journal
+
+candidates = [
+    {
+        "data": dict(
+            journal_title="La vie spirituelle",
+            type="per",
+            year=1930,
+            author="Daniélou",
+            title=None,
+            publisher=None,
+        ),
+        "query": 'bib.title all "la vie spirituelle" and bib.recordtype all "per" and bib.publicationdate all "1930" and bib.author "Daniélou"',
+    }
+]
 
 
-def test_human():
-    jon_snow = Human(name="Jon Snow")
-    assert isinstance(jon_snow, Human) == True
+def test_article():
+    a = Article(
+        journal_title="La vie spirituelle",
+        pages=list(range(135, 138)),
+        title="Pour lire saint Augustin",
+        author="Daniélou",
+    )
+    assert isinstance(a, Article)
+    assert isinstance(a._source(), Journal)
+    assert (
+        a.get_query()
+        == 'bib.title all "La vie spirituelle" and bib.recordtype all "per"'
+    )
