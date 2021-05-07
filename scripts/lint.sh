86a7cdaa083a00b1be3a3c0c11e22dd08eb4f907
John Maximilian <2e0byo@gmail.com>
2021-05-07 20:16:52 +0100

    run mypy last

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy gallica-autobib --disallow-untyped-defs
-black gallica-autobib tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty gallica-autobib gallica-autobib tests
+black gallica_autobib tests --check
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty gallica_autobib gallica_autobib tests
+mypy gallica_autobib --disallow-untyped-defs
