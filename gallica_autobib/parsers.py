3c38b2313f383adc9eb41c3cfec139f3f097b24d
John Maximilian <2e0byo@gmail.com>
2021-05-07 22:58:01 +0100

    roman

--- gallica_autobib/parsers.py
+++ gallica_autobib/parsers.py
@@ -4,6 +4,7 @@ import rispy
 from .models import Article, Book, Collection
 from typing import Union, List
 from devtools import debug
+import roman
 
 
 class ParsingError(Exception):
@@ -18,13 +19,16 @@ def parse_bibtex(bibtex: str) -> List[Union[Article, Book, Collection]]:
     parsed = []
     for record in db.entries:
         pages = record["pages"]
-        if "i" in pages.lower():
-            raise NotImplementedError("Don't yet handle roman numerals")
+        roman = "i" in pages.lower()
         try:
             start, end = pages.split("-")
-            if int(end) < int(start):
+            startno = roman.fromRoman(start) if roman else int(start)
+            endno = roman.fromRoman(end) if roman else int(end)
+            if not roman and endno < startno:
                 end = f"{start[0]}{end}"
-            record["pages"] = list(range(int(start), int(end) + 1))
+            record["pages"] = list(range(startno, endno + 1))
+            if roman:
+                record["pages"] = [roman.toRoman(x) for x in record["pages"]]
         except ValueError:
             record["pages"] = [record["pages"]]
 
@@ -40,8 +44,13 @@ def parse_bibtex(bibtex: str) -> List[Union[Article, Book, Collection]]:
 def parse_ris(ris: str) -> List[Union[Article, Book, Collection]]:
     db = rispy.loads(ris)
     for record in db:
-        record["author"] = record["first_authors"]
-        mapping = {"ART": Article, "BOOK": Book, "COLL": Collection}
+        debug(record)
+        record["pages"] = list(
+            range(int(record["start_page"]), int(record["end_page"]) + 1)
+        )
+        record["author"] = "and".join(record["authors"])
+        record["journaltitle"] = record["secondary_title"]
+        mapping = {"JOUR": Article, "BOOK": Book, "COLL": Collection}
         type_ = record["type_of_reference"]
         if type_ in mapping:
             return mapping[type_].parse_obj(record)
