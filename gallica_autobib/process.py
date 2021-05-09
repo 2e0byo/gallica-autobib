1a8fe56ea8ff8bae9a17d49bd59169ee868c7ea8
John Maximilian <2e0byo@gmail.com>
2021-05-09 18:15:55 +0100

    better test coverage (no tiff for now as can't find any)

--- gallica_autobib/process.py
+++ gallica_autobib/process.py
@@ -13,13 +13,14 @@ from PyPDF4.pdf import PageObject
 from PIL import Image
 from typing import Tuple, Any
 
+from io import BytesIO
+
 
 class ExtractionError(Exception):
     pass
 
 
 def extract_image(page: PageObject) -> Tuple[Any, str]:
-
     if "/XObject" in page["/Resources"]:
         xObject = page["/Resources"]["/XObject"].getObject()
 
@@ -33,18 +34,24 @@ def extract_image(page: PageObject) -> Tuple[Any, str]:
                     mode = "P"
 
                 if "/Filter" in xObject[obj]:
-                    if xObject[obj]["/Filter"] == "/FlateDecode":
+                    filter_ = xObject[obj]["/Filter"][0]
+
+                    if filter_ == "/FlateDecode":
                         data = Image.frombytes(mode, size, data)
                         type_ = "png"
-                    elif xObject[obj]["/Filter"] == "/DCTDecode":
+                    elif filter_ == "/DCTDecode":
                         type_ = "jpg"
-                    elif xObject[obj]["/Filter"] == "/JPXDecode":
+                    elif filter_ == "/JPXDecode":
                         type_ = "jp2"
-                    elif xObject[obj]["/Filter"] == "/CCITTFaxDecode":
+                    elif filter_ == "/CCITTFaxDecode":
                         type_ = "tiff"
                 else:
                     type_ = "png"
                     data = Image.frombytes(mode, size, data)
+                if isinstance(data, bytes):
+                    data = Image.open(BytesIO(data))
+                assert data
+                assert type_
                 return data, type_
     else:
         raise ExtractionError("No image found.")
--- tests/test_process.py
+++ tests/test_process.py
@@ -2,14 +2,9 @@ from gallica_autobib.process import extract_image, ExtractionError
 import pytest
 from PyPDF4 import PdfFileReader
 from pathlib import Path
-
-
-def test_extract_image(image_regression):
-    with Path("tests/test_process/test.pdf").open("rb") as f:
-        reader = PdfFileReader(f)
-        page1 = reader.getPage(1)
-        img, type_ = extract_image(page1)
-    image_regression.check(img)
+from collections import namedtuple
+from tempfile import TemporaryDirectory
+from devtools import debug
 
 
 def test_extract_no_image():
@@ -17,3 +12,27 @@ def test_extract_no_image():
         reader = PdfFileReader("tests/test_process/test-blank.pdf")
         page1 = reader.getPage(0)
         extract_image(page1)
+
+
+ImgTest = namedtuple("ImgTest", ("testfile", "type"))
+
+test_image_pdf = [
+    ImgTest(Path("tests/test_process/test5.pdf"), "png"),
+    ImgTest(Path("tests/test_process/test2.pdf"), "jpg"),
+    ImgTest(Path("tests/test_process/test3.pdf"), "jp2"),
+    # ImgTest(Path("tests/test_process/test4.pdf"), "tiff"),
+]
+
+
+@pytest.mark.parametrize("img_test", test_image_pdf)
+def test_extract_image(img_test, image_regression):
+    with img_test.testfile.open("rb") as f:
+        reader = PdfFileReader(f)
+        page1 = reader.getPage(0)
+        img, type_ = extract_image(page1)
+        assert type_ == img_test.type
+    with TemporaryDirectory() as tmpdir:
+        outf = Path(f"{tmpdir}/test.{type_}")
+        img.save(str(outf))
+        with outf.open("rb") as f:
+            image_regression.check(f.read())
