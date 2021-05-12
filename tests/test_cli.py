b0e7578683878a723c28229d2efd8f1cb5b2cf1f
John Maximilian <2e0byo@gmail.com>
2021-05-12 10:14:23 +0100

    start testing cli

---
+++ tests/test_cli.py
@@ -0,0 +1,17 @@
+from typer.testing import CliRunner
+from devtools import debug
+
+from gallica_autobib.cli import app
+
+runner = CliRunner()
+
+
+def test_app(tmp_path, file_regression):
+    result = runner.invoke(app, ["tests/test_cli/test.bib", str(tmp_path)])
+    debug(result.stdout)
+    try:
+        debug(result.stderr)
+    except:
+        pass
+    assert result.exit_code == 0
+    file_regression.check(result.stdout)
