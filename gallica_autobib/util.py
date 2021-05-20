85de66863d8a05cc4f1e16fb07255ba3ccc329c4
John Maximilian <2e0byo@gmail.com>
2021-05-20 20:42:55 +0100

    prettifyer

--- gallica_autobib/util.py
+++ gallica_autobib/util.py
@@ -1,5 +1,58 @@
 from typing import Union
+import roman
 
 
-def pretty_page_range(pages: list[Union[str, int]]):
+def pretty_page_range(pages: list[Union[str, int]]) -> str:
     """Prettify a page range."""
+    ranges = []
+    try:
+        int(pages[0])
+        arabic = True
+    except ValueError:
+        arabic = False
+
+    pp = []
+    for p in pages:
+        if arabic:
+            try:
+                pp.append(int(p))
+            except ValueError:
+                ranges.append(dict(arabic=arabic, pages=pp))
+                arabic = False
+                pp = [roman.fromRoman(p.upper())]
+        else:
+            try:
+                pp.append(roman.fromRoman(p.upper()))
+            except roman.InvalidRomanNumeralError:
+                ranges.append(dict(arabic=arabic, pages=pp))
+                arabic = True
+                pp = [p]
+    ranges.append(dict(arabic=arabic, pages=pp))
+
+    pretty = []
+    for r in ranges:
+        pp = [r["pages"][0]]
+        arabic = r["arabic"]
+        for p in r["pages"][1:]:
+            if p == pp[-1] + 1:
+                pp.append(p)
+            else:
+                pretty.append(prettify(pp, arabic))
+                pp = [p]
+        pretty.append(prettify(pp, arabic))
+
+    return "pp. " + ", ".join(pretty)
+
+
+def prettify(pages: list[int], arabic: bool) -> str:
+    """Pages is a continuous range of ints."""
+    if arabic:
+        start = str(pages[0])
+        end = str(pages[-1])
+        if len(start) == len(end):
+            end = "".join(end[i] for i in range(len(end)) if end[i] != start[i])
+        return f"{start}--{end}"
+    else:
+        # for now we don't do anything clever with roman numerals, although
+        # combining is possible.
+        return f"{roman.toRoman(pages[0]).lower()}--{roman.toRoman(pages[-1]).lower()}"
--- tests/test_util.py
+++ tests/test_util.py
@@ -4,10 +4,11 @@ from gallica_autobib import util
 
 page_ranges = [
     [[str(i) for i in range(1, 11)], "pp. 1--10"],
-    [[str(i) for i in range(450, 456)], "pp. 450--7"],
+    [[str(i) for i in range(450, 456)], "pp. 450--5"],
     [[str(i) for i in range(450, 470)], "pp. 450--69"],
     [["10", "11", "12", "13", "17", "18", "19"], "pp. 10--3, 17--9"],
-    [["i", "ii", "iii"], "pp. i-iii"],
+    [["i", "ii", "iii"], "pp. i--iii"],
+    [["1", "2", "i", "ii", "iii"], "pp. 1--2, i--iii"],
 ]
 
 
