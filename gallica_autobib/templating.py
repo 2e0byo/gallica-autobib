fb9a83d3adaea254b534fb6960558f4e3526f2ad
John Maximilian <2e0byo@gmail.com>
2021-05-20 11:41:31 +0100

    escapes

--- gallica_autobib/templating.py
+++ gallica_autobib/templating.py
@@ -2,11 +2,11 @@ from jinja2 import Environment, PackageLoader, Template, select_autoescape
 
 
 latex_env = Environment(
-    block_start_string="\BLOCK{",
+    block_start_string="\\BLOCK{",
     block_end_string="}",
-    variable_start_string="\VAR{",
+    variable_start_string="\\VAR{",
     variable_end_string="}",
-    comment_start_string="\#{",
+    comment_start_string="\\#{",
     comment_end_string="}",
     line_statement_prefix="%%",
     line_comment_prefix="%#",
