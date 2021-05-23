7df0901ba998683b18833d012abfd12c07fd4088
John Maximilian <2e0byo@gmail.com>
2021-05-23 17:15:15 +0100

    config and branch coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,5 @@
 set -e
 set -x
 
-pytest --cov=gallica_autobib --cov=tests --cov-report=term-missing ${@} --cov-report xml
+pytest --cov=gallica_autobib --cov=tests --cov-report=term-missing ${@} --cov-report xml --cov-config .coveragerc --cov-branch
 bash ./scripts/lint.sh
