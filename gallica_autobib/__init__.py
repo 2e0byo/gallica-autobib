648103aaf37a03c99f496bfc9abd6a3ada723c44
John Maximilian <2e0byo@gmail.com>
2022-07-11 18:50:25 +0100

    feat: global as_str fn.

--- gallica_autobib/__init__.py
+++ gallica_autobib/__init__.py
@@ -1,7 +1,16 @@
 from importlib.metadata import version
+from typing import Iterable, Union
 
 import coloredlogs
 
 __version__ = version(__name__)
 
 coloredlogs.install()
+
+
+def as_string(x: Union[str, Iterable]):
+    """Convert something to a string."""
+    if isinstance(x, str):
+        return x
+    else:
+        return " ".join(x)
